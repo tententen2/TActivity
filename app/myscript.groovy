08824e01e3f62e6040dd16701e79b72456550073
Jirawat.Poo <pps.jirawat@gmail.com>
2018-04-19 17:17:44 +0700

    dqwdqwdq

--- app/myscript.groovy
+++ app/myscript.groovy
@@ -1,6 +1,6 @@
-def command = "git submodule init"
-def command2 = "git submodule update"
+def command = "git submodule foreach git pull origin master"
+//def command2 = "git submodule update"
 def proc = command.execute()
-def proc2 = command2.execute()
-proc.waitFor()
+//def proc2 = command2.execute()
+//proc.waitFor()
 proc2.waitFor()
