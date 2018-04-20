e00c72a4bf90bee460ac00e02f7966b7b0ec4c36
Jirawat.Poo <pps.jirawat@gmail.com>
2018-04-20 12:27:43 +0700

    dwqdwq

--- app/myscript.groovy
+++ app/myscript.groovy
@@ -1,9 +1,1 @@
-def init = "git submodule init"
-def command = "git submodule foreach git pull origin master"
-def command2 = "git submodule update"
-def proc2 = init.execute()
-def proc = command.execute()
-def dskao = command2.execute()
-proc2.waitFor()
-proc.waitFor()
-dskao.waitFor()
+"git submodule update --remote".execute().waitFor()
